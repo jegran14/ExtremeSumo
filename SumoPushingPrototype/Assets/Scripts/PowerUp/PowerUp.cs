e08413319d788aaacbb80aa2b1fd8f9ecd7345d8
Chepriav <al341533@uji.es>
2018-04-15 19:28:24 +0200

    PowerUp y corrutina para mantenerlo por un tiempo

---
+++ SumoPushingPrototype/Assets/Scripts/PowerUp/PowerUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+    private float force;
+    private PlayerController player;
+    public GameObject ball;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag.Contains("Player"))
+        {
+            player = other.GetComponent<PlayerController>();
+            player.pushForce = 50;
+            ball.SetActive(false);
+            
+            StartCoroutine("CountDown");
+            
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds (5f);
+        player.pushForce = 10;
+        Destroy(gameObject);
+    }
+}
