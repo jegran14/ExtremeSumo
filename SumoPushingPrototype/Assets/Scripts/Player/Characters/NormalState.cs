c4202d1ca699b5c724c6dcd3731d604552315e56
Vicent Santamarta <vicent.santamarta@gmail.com>
2018-04-15 14:05:47 +0200

    Juntado el empuje con el control con mando
    Faltan pulir y aclarar el funcionamiento del empuje

--- SumoPushingPrototype/Assets/Scripts/Player/Characters/NormalState.cs
+++ SumoPushingPrototype/Assets/Scripts/Player/Characters/NormalState.cs
@@ -8,7 +8,7 @@ public class NormalState : ICharacterState
     private PlayerController thePlayer;
     private Vector3 movementInput;
 
-    public NormalState(PlayerController player, Rigidbody rb)
+    public NormalState(PlayerController player)
     {
         thePlayer = player;
     }
@@ -21,27 +21,32 @@ public class NormalState : ICharacterState
 
     public void UpdateState()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal" + thePlayer.player);
+        float vertical = Input.GetAxis("Vertical" + thePlayer.player);
 
         movementInput = new Vector3(horizontal, 0.0f, vertical);
         movementInput = movementInput.normalized;
+
+        if (Input.GetButtonDown("Jump" + thePlayer.player))
+        {
+            thePlayer.Push();
+            return;
+        }
     }
 
     private void Move()
     {
         Vector3 movement = movementInput * thePlayer.movementSpeed * Time.deltaTime;
         thePlayer.Move(movement);
-
-        
     }
 
     private void Turn()
     {
         if(movementInput != Vector3.zero)
         {
-            Quaternion rotation = Quaternion.SlerpUnclamped(thePlayer.transform.rotation, Quaternion.LookRotation(movementInput), thePlayer.turnSpeed * Time.deltaTime);
-            thePlayer.Turn(rotation);
+            //Quaternion rotation = Quaternion.SlerpUnclamped(thePlayer.transform.rotation, Quaternion.LookRotation(movementInput), thePlayer.turnSpeed * Time.deltaTime);
+            // thePlayer.Turn(rotation);
+            thePlayer.Turn(Quaternion.LookRotation(movementInput));
         }
     }
 
--- SumoPushingPrototype/Assets/Scripts/Player/PlayerController.cs
+++ SumoPushingPrototype/Assets/Scripts/Player/PlayerController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    //Player Number
+    public int player;
+
     //Movement paramenters
     public float movementSpeed = 10f;
-    public float turnSpeed = 300f;
+    public float turnSpeed = 20f;
+    public float pushForce = 10f;
+
     //Character States
     [HideInInspector] public NormalState normalState;
+    [HideInInspector] public PushState pushState;
     private ICharacterState currentState; //The current state the character is on
+
     //Collision layers
-    private int pushableLayer;
-    private int groundLayer;
+    [HideInInspector]
+    public int pushableLayer;
+    [HideInInspector]
+    public int groundLayer;
+
     //Character components
     private Rigidbody rb;
     private Animator anim;
@@ -20,8 +30,10 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
 
-        normalState = new NormalState(this, rb);
+        normalState = new NormalState(this);
+        pushState = new PushState(this);
         currentState = normalState;
 
         pushableLayer = LayerMask.GetMask("Pushable");
@@ -41,11 +53,44 @@ public class PlayerController : MonoBehaviour {
     public void Move(Vector3 movement)
     {
         rb.MovePosition(rb.position + movement);
+
+        if (movement != Vector3.zero)
+        {
+            anim.SetBool("Walking", true);
+        }     
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
     }
 
     public void Turn(Quaternion rot)
     {
         //Quaternion rotation = Quaternion.LerpUnclamped(rot, rb.rotation, turnSpeed*Time.deltaTime);
         rb.MoveRotation(rot);
+    }
+
+    public void Push()
+    {
+        currentState = pushState;
+        pushState.Push();
+    }
+
+    public void ToNormal()
+    {
+        currentState = normalState;
+    }
+
+    public void Pushed(Vector3 direction, float force)
+    {
+        Vector3 dir;
+        if (direction != Vector3.zero)
+            dir = direction;
+        else
+            dir = transform.forward;
+
+        rb.AddForce(transform.forward * force, ForceMode.Impulse);
+
+        ToNormal();
     }
 }
