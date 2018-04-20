68292d6678c8aabf4decbd7d36728c155c04417c
danzel92 <33261967+danzel92@users.noreply.github.com>
2018-04-20 02:00:10 +0200

    Nuevo nivel y Texturas
    Creación del nivel 3 (Lava).
    
    Tambien se han añadido texturas a las plataformas para hacerlas mas interesantes a nivel visual.

---
+++ SumoPushingPrototype/Assets/Scripts/Escenario/Desintegracion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desintegracion : MonoBehaviour {
+
+    public float velocity=1;
+    public float minSize = 0.3f;
+
+    private float x;
+    private float y;
+    private float z;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (transform.localScale.y> minSize)
+        {
+            x -= 0.1f * Time.deltaTime * velocity;
+            y -= 0.1f * Time.deltaTime * velocity;
+            z -= 0.1f * Time.deltaTime * velocity;
+            transform.localScale += new Vector3(x, y, z);
+        }
+
+
+    }
+}
