0d254277f788de36436ad5b4915de9345a9bf507
Chepriav <al341533@uji.es>
2018-04-21 20:21:51 +0200

    Navigation mesh

---
+++ SumoPushingPrototype/Assets/Scripts/Player/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Transform player;
+    NavMeshAgent nav;
+
+	// Use this for initialization
+	void Awake () {
+        player = GameObject.FindGameObjectsWithTag("Player1").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
