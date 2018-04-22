83b46664a17fe0d58b35d487feb82e3cac6bf531
Chepriav <al341533@uji.es>
2018-04-22 13:17:07 +0200

    EnemyController

--- SumoPushingPrototype/Assets/Scripts/Player/EnemyMovement.cs
+++ SumoPushingPrototype/Assets/Scripts/Player/EnemyMovement.cs
@@ -3,18 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class EnemyMovement : MonoBehaviour {
 
-    Transform player;
+    GameObject [] players;
     NavMeshAgent nav;
+    Transform player;
+    Vector3 position;
+    float distance;
+
 
 	// Use this for initialization
 	void Awake () {
-        player = GameObject.FindGameObjectsWithTag("Player1").transform;
+        players = GameObject.FindGameObjectsWithTag("Player1");
+
+        nav = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        position = gameObject.transform.position;
+        distance = Mathf.Infinity;
+
+	    for (int i = 0; i < players.Length; i++)
+        {
+            var diference = (position - players[i].transform.position);
+            var currentDistance = diference.sqrMagnitude;
+            if(currentDistance < distance)
+            {
+                player = players[i].transform;
+                distance = currentDistance;
+            }
+        }
+
+        nav.SetDestination(player.position);
 	}
 }
