9e6368239c96890faae579703ce0f5527acafc2e
danzel92 <33261967+danzel92@users.noreply.github.com>
2018-04-25 14:41:49 +0200

    Cambios Esteticos y mecanicos de los niveles
    Cambio estetico del nivel 2, ahora la arena tiene un mayor aspecto de congelacion.
    
    Se ha mejorado la mecanica de desintegracion.

--- SumoPushingPrototype/Assets/Scripts/Escenario/Desintegracion.cs
+++ SumoPushingPrototype/Assets/Scripts/Escenario/Desintegracion.cs
@@ -4,28 +4,36 @@ using UnityEngine;
 
 public class Desintegracion : MonoBehaviour {
 
-    public float velocity=1;
-    public float minSize = 0.3f;
+    public float velocityEscale = 1;
+    public float velocityPosition = 1;
+    public float minSize = 10f;
 
     private float x;
     private float y;
     private float z;
 
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
-    void Update() {
-        if (transform.localScale.y> minSize)
+    void Update()
+    {
+
+        if (transform.localScale.x > minSize)
         {
-            x -= 0.1f * Time.deltaTime * velocity;
-            y -= 0.1f * Time.deltaTime * velocity;
-            z -= 0.1f * Time.deltaTime * velocity;
-            transform.localScale += new Vector3(x, y, z);
+            x -= 0.1f * Time.deltaTime * velocityEscale;
+
+            z -= 0.1f * Time.deltaTime * velocityEscale;
+            transform.localScale += new Vector3(x, 0, z);
+
+            if (transform.localPosition.y > -10)
+            {
+                y -= 0.1f * Time.deltaTime * velocityPosition;
+                transform.localPosition += new Vector3(0, y, 0);
+            }
         }
-
-
     }
 }
