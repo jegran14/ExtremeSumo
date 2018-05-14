6f4636e4158edbae7779016245046a35d139dbbe
Chepriav <al341533@uji.es>
2018-05-14 16:56:54 +0200

    fx balls

--- SumoPushingPrototype/Assets/Scripts/PowerUp/PowerUp.cs
+++ SumoPushingPrototype/Assets/Scripts/PowerUp/PowerUp.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class PowerUp : MonoBehaviour {
     private float force;
     private PlayerController player;
+    private Vector3 ballPosition;
     public GameObject ball;
     public ModoHulk modoHulk;
     public ModoPitufo modoPitufo;
+    public float rotate;
+    public float amplitudeX;
+    public float amplitudeY;
+    public float omegaX;
+    public float omegaY;
+    float index;
 
     private void Start()
     {
-        
-        modoHulk = GetComponent<ModoHulk>();
+        ballPosition = ball.transform.position;
+    }
+    private void Update()
+    {
+        index += Time.deltaTime;
+        float x = amplitudeX * Mathf.Cos(omegaX * index);
+        float y = Mathf.Abs(amplitudeY * Mathf.Sin(omegaY * index));
+        ball.transform.localPosition = new Vector3(x, y, 0);
+
+        ball.transform.Rotate(Vector3.up, -rotate * Time.deltaTime);
+        Debug.Log(Quaternion.Euler(1f,rotate,1f));
     }
     private void OnTriggerEnter(Collider other)
     {
