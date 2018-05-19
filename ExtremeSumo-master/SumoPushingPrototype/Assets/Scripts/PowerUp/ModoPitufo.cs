d8a34052bd2a2d9762d8ed47819b8c5b930f271c
Chepriav <al341533@uji.es>
2018-05-19 14:40:59 +0200

    Cambios en power ups

--- ExtremeSumo-master/SumoPushingPrototype/Assets/Scripts/PowerUp/ModoPitufo.cs
+++ ExtremeSumo-master/SumoPushingPrototype/Assets/Scripts/PowerUp/ModoPitufo.cs
@@ -8,6 +8,8 @@ public class ModoPitufo : MonoBehaviour {
     public float time;
     public float force;
     public Vector3 scale;
+    private PowerUp power;
+
     // Use this for initialization
     public void StartPowerUp(PlayerController player)
     {
@@ -34,7 +36,9 @@ public class ModoPitufo : MonoBehaviour {
         Debug.Log("Reducir");
         player.transform.localScale += scale;
         player.pushForce += force;
+        power.active = false;
         //Devolver a condiciones normales
     }
+
 }
 
---
+++ ExtremeSumo-master/SumoPushingPrototype/Assets/Scripts/PowerUp/SpawnPowerUp.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPowerUp : MonoBehaviour {
+
+    public GameObject powerUp;
+  
+
+    public float maxTime = 25;
+    public float minTime = 10;
+
+    //Posiciones
+    private float x;
+    private float y;
+    private float z;
+    private Vector3 pos;
+
+    private GameObject powerClone;
+    //Tiempo actual
+    private float time;
+
+    //Booleano para esperar a fin de powerup
+    [HideInInspector]
+    public bool finish;
+
+    //Tiempo de spawneo
+    private float spawnTime;
+
+	// Use this for initialization
+	void Start () {
+        SetRandomTime();
+        y = 0.5f;
+        finish = true;
+	}
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        //Contador
+        if (finish) { 
+            time += Time.deltaTime;
+        }
+        
+        //Chequear que esta en el tiempo adecuado de spawneo.
+        if (time >= spawnTime)
+        {
+            SpawnObject();
+        }   
+	}
+
+    private void SpawnObject()
+    {
+        x = Random.Range(-7, 7);
+        z = Random.Range(-7, 7);
+        pos = new Vector3(x, y, z);
+
+        finish = false;
+        time = 0;
+        transform.position = pos;
+        powerClone = Instantiate(powerUp, transform.position, powerUp.transform.rotation);
+        powerClone.SetActive(true);
+        
+    }
+
+    private void SetRandomTime()
+    {
+        time = 0;
+        spawnTime = Random.Range(minTime, maxTime);
+    }
+}
